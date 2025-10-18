26bf1d304879d664ca8fe158f6ebbe87c28fe5f8
Armaan Badhan <armaanbadhan07@gmail.com>
2025-10-18 13:12:23 +0530

    Initial Commit

---
+++ Snapchat Memory Jar/Snapchat_Memory_JarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snapchat_Memory_JarApp.swift
+//  Snapchat Memory Jar
+//
+//  Created by armaan badhan on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Snapchat_Memory_JarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
